8d1dd635224819c0ca21dcd3281e6fdf352bc3d7
JairAntonio22 <jair.loranca@gmail.com>
2022-11-04 19:16:40 -0600

    Experiment results

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import numpy as np
+import pandas as pd
 
 from ttp import *
-from glob import glob
-from pprint import pprint
 
 
-if __name__ == '__main__':
-    filenames = glob('TTP Instances/berlin52-ttp/berlin52_n51_*')
-    filenames.sort()
+instances = pd.read_csv('instance_results.csv', index_col='instance')
+results = {'solver': [], 'best': []}
 
-    for filename in filenames[:1]:
-        ttp = load_ttp(filename)
+for name, row in instances.iterrows():
+    ttp = load_ttp('instances/' + name)
+    ttp.solve('default')
+    score = ttp.eval()
 
-        ttp.solve('default')
-        ttp.show_results()
+    if score < row['profit']:
+        results['solver'].append(score)
+        results['best'].append(row['profit'])
+
+df = pd.DataFrame(results)
+df['error'] = 100 * np.abs(df['solver'] - df['best']) / df['best']
+print(df['error'].describe().T)
